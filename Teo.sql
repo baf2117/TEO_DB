30068b9bee65f0683a6b065e4f306a9eefe0fe1b
baf2117 <baf.0017@gmail.com>
2020-03-10 20:32:23 -0600

    Update
    Agregando tabla noticias

--- Teo.sql
+++ Teo.sql
@@ -61,6 +61,20 @@ CREATE TABLE IF NOT EXISTS `Clase` (
   CONSTRAINT `Clase_users_FK` FOREIGN KEY (`id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=270 DEFAULT CHARSET=latin1;
 
+CREATE TABLE IF NOT EXISTS `Noticia` (
+  `id_noticia` int(11) NOT NULL AUTO_INCREMENT,
+  `Titulo` varchar(30) DEFAULT NULL,
+  `Descripcion` varchar(1000) DEFAULT NULL,
+  `Fecha` datetime DEFAULT NULL,
+  `id_clase` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  PRIMARY KEY (`id_noticia`),
+  KEY `Noticia_Clase_FK` (`id_clase`),
+  KEY `Noticia_users_FK` (`id`),
+  CONSTRAINT `Noticia_Clase_FK` FOREIGN KEY (`id_clase`) REFERENCES `Clase` (`id_clase`),
+  CONSTRAINT `Noticia_users_FK` FOREIGN KEY (`id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=122 DEFAULT CHARSET=latin1;
+
 CREATE TABLE IF NOT EXISTS `Dialogo` (
   `id_dialogo` int(11) NOT NULL AUTO_INCREMENT,
   `Fecha` datetime DEFAULT NULL,
